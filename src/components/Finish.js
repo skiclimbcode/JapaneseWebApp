f66be2a71218874954ba21c15bddfd593bb9cc0b
Adrian Leonard <leonard.adrian96@gmail.com>
2021-01-23 09:36:27 -0700

    Cleaning up Finish.js and making it look better

--- src/components/Finish.js
+++ src/components/Finish.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 export default function Finish(props) {
 
@@ -10,12 +13,34 @@ export default function Finish(props) {
 
     return (
     <React.Fragment>
-        <div> You're done! </div>
-        <div> Correct Responses: <span className="correct-color">{props.correct}</span></div>
-        <div> Mistakes: <span className="mistakes-color">{props.mistakes}</span></div>
-        <div> You scored {props.correct - props.mistakes} points!</div>
-        <div> It took you an average of {roundOff((props.guessTimes.reduce((a, b) => a + b) / props.guessTimes.length) / 1000, 2)} seconds to guess each character!</div>
-        <Button variant="primary" onClick={props.restart}>Go Again</Button>
+        <Container>
+            <Row>
+                <Col xs={12}><h2>Finished!</h2></Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={6} className="left-align">Correct:</Col>
+                <Col xs={12} sm={6} className="right-align correct-color">{props.correct}</Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={6} className="left-align">Mistakes:</Col>
+                <Col xs={12} sm={6} className="right-align mistakes-color">{props.mistakes}</Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={6} className="left-align">Score:</Col>
+                <Col xs={12} sm={6} className="right-align correct-color">{props.correct - props.mistakes}</Col>
+            </Row>
+            <Row>
+                <Col xs={12} sm={6} className="left-align">Avg Guess:</Col>
+                <Col xs={12} sm={6} className="right-align correct-color">
+                    {roundOff((props.guessTimes.reduce((a, b) => a + b) / props.guessTimes.length) / 1000, 2)} s    
+                </Col>  
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <Button variant="primary" onClick={props.restart}>Go Again</Button>
+                </Col>
+            </Row>
+        </Container>
     </React.Fragment>
     )
 }
